74b85285a74af9e9a01e2337226dfd0ef5bc4d0d
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-22 23:44:15 +0600

    express server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,22 +2,19 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const post = orm.em.fork({}).create(Post, {
-    title: "my first post",
-  });
-  await orm.em.persistAndFlush(post);
-
-  // const post = orm.em.create(Post, {title: "my first post"});
-  // console.log('-------------sql------------');
-  // await orm.em.nativeInsert(Post, {title: 'my second post'});
-
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send("hello");
+  })
+  app.listen(4000, () => {
+    console.log('Server started on localhost:4000');
+  })
 };
 
 main().catch((err) => {
