4f7d149ef48c3530e61318a97c7fc6d5995a7487
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-28 16:20:58 +0600

    urql provider wrapper

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
-
-import theme from '../theme'
 import { AppProps } from 'next/app'
+import { createClient, Provider } from 'urql'
+import theme from '../theme'
+
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   )
 }
 
