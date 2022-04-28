c63afb10a0570a67b2446dcdcf6ccc27707ba734
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-28 17:43:42 +0600

    register error mappig

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -4,18 +4,26 @@ import { Box, Button } from '@chakra-ui/react';
 import Wrapper from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 interface registerProps {};
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
   return (
     <Wrapper variant='small'>
       <Formik 
         initialValues={{ username: "", password: "" }} 
-        onSubmit={async(values) => {
+        onSubmit={async(values, { setErrors }) => {
           const response = await register(values);
-          response.data.register?.user.id
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // Worked
+            router.push("/");
+          }
         }}
       >
         {( isSubmitting ) => (
