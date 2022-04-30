4a331d9856de0938f6e32c147aadce8c4deff58b
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-30 23:48:04 +0600

    Logout functionality, clears cookie recieved from the apollo server, still cannot generate cookie from the frontend call

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -34,7 +34,8 @@ const main = async () => {
   
   app.use(
     session({
-      name: 'qid',
+      name: COOKIE_NAME,
+      // name: 'qid',
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -3,6 +3,7 @@ import { User } from "../entities/User";
 import { MyContext } from "src/types";
 import argon2 from "argon2";
 import { EntityManager } from '@mikro-orm/postgresql';
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UPInput {         // UP = Username Password
@@ -136,5 +137,23 @@ export class UserResolver {
     return {
       user
     };
+  }
+
+  @Mutation(() => Boolean)
+  logout(
+    @Ctx() { req, res }: MyContext
+  ) {
+    return new Promise((resolve) => 
+      req.session.destroy(err => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        };
+
+        resolve(true);
+      })
+    );
   }
 }
