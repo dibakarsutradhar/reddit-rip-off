36145fa0d687ca321a1cde4c0b6f248e004c253f
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-11 00:05:32 +0600

    refactored useEffect to its own utils

--- client/src/pages/create-post.tsx
+++ client/src/pages/create-post.tsx
@@ -1,15 +1,17 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import React from "react";
 import { InputField } from "../components/InputField";
-import { useCreatePostMutation } from "../generated/graphql";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
 import Layout from "../components/Layout";
+import { useCreatePostMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { useIsAuth } from "../utils/useIsAuth";
 
 const CreatePost: React.FC<{}> = ({}) => {
   const router = useRouter();
+  useIsAuth();
   const [, createPost] = useCreatePostMutation();
   return (
     <Layout variant="small">
