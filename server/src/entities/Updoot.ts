ffa84596d12d3b3eceab255bec6faebfb49077e2
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-20 01:07:39 +0600

    upvote backend

--- server/src/entities/Updoot.ts
+++ server/src/entities/Updoot.ts
@@ -1,33 +1,26 @@
-import { Field, ObjectType } from "type-graphql";
 import { BaseEntity, Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
-import { User } from "./User";
 import { Post } from "./Post";
+import { User } from "./User";
 
 // Many to Many relationship
 // user <-> posts
 // user -> join table <- posts
 // user -> updoot <- posts
 
-@ObjectType()
 @Entity()
 export class Updoot extends BaseEntity {
-  @Field()
   @Column({ type: "int" })
   value: number;
 
-  @Field()
   @PrimaryColumn()
   userId: number;
 
-  @Field()
   @ManyToOne(() => User, (user) => user.updoots)
-  user: number;
+  user: User;
 
-  @Field()
   @PrimaryColumn()
   postId: number;
 
-  @Field()
   @ManyToOne(() => Post, (post) => post.updoots)
   post: Post;
 }
