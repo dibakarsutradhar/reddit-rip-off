dcad700ed6036b141e461c13b2f85cc747edfb0a
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-25 01:45:17 +0600

    upvoting downvoting sql

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -14,7 +14,7 @@ import {
 } from "type-graphql";
 import dataSource from "../app-data-source";
 import { Post } from "../entities/Post";
-// import { Updoot } from "../entities/Updoot";
+import { Updoot } from "../entities/Updoot";
 import { isAuth } from "../middleware/isAuth";
 import { MyContext } from "../types";
 
@@ -52,26 +52,51 @@ export class PostResolver {
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
 
-    // Updoot.insert({
-    //   userId,
-    //   postId,
-    //   value: realValue,
-    // });
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-    await dataSource.query(
-      `
-      START TRANSACTION;
+    // the user has voted on the post before
+    // and they are changing their voite
+    if (updoot && updoot.value !== realValue) {
+      await dataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+        update updoot
+        set value = $1
+        where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
 
-      insert into updoot ("userId", "postId", value)
-      values(${userId}, ${postId}, ${realValue});
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2
+        `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      // has never voted before
+      await dataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+        insert into updoot ("userId", "postId", value)
+        values($1, $2, $3)
+        `,
+          [userId, postId, realValue]
+        );
 
-      update post
-      set points = points + ${realValue}
-      where id = ${postId};
-
-      COMMIT;
-    `
-    );
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2
+        `,
+          [realValue, postId]
+        );
+      });
+    }
 
     return true;
   }
