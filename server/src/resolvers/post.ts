01647e731ecac8aa885fe02fa629fffd54f20e23
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-05-25 02:42:28 +0600

    ssr cookie forwarding

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -110,10 +110,17 @@ export class PostResolver {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
 
-    const replacements: any[] = [realLimitPlusOne, req.session.userId];
+    const replacements: any[] = [realLimitPlusOne];
+
+    if (req.session.userId) {
+      replacements.push(req.session.userId);
+    }
+
+    let cursorIdx = 3;
 
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
+      cursorIdx = replacements.length;
     }
 
     const posts = await dataSource.query(
@@ -133,7 +140,7 @@ export class PostResolver {
       }
       from post p
       inner join public.user u on u.id = p."creatorId"
-      ${cursor ? `where p."createdAt" < $3` : ""}
+      ${cursor ? `where p."createdAt" < $${cursorIdx}` : ""}
       order by p."createdAt" DESC
       limit $1
     `,
